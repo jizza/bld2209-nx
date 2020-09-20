422f89597201c2c571a243ae39404d19087d5933
Tijmen Amsing <tijmen.amsing@capgemini.com>
2020-09-21 00:07:26 +0200

    fix lint

--- school/apps/student-app/src/app/app.component.spec.ts
+++ school/apps/student-app/src/app/app.component.spec.ts
@@ -1,14 +1,16 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { async, TestBed } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [AppComponent],
-      schemas: [NO_ERRORS_SCHEMA],
-    }).compileComponents();
-  }));
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [AppComponent],
+        schemas: [NO_ERRORS_SCHEMA],
+      }).compileComponents();
+    })
+  );
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
--- school/libs/student/feature-view-assignments/src/lib/view-assignment/view-assignment.component.spec.ts
+++ school/libs/student/feature-view-assignments/src/lib/view-assignment/view-assignment.component.spec.ts
@@ -1,17 +1,19 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { ViewAssignmentComponent } from './view-assignment.component';
 
 describe('ViewAssignmentComponent', () => {
   let component: ViewAssignmentComponent;
   let fixture: ComponentFixture<ViewAssignmentComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ViewAssignmentComponent],
-      schemas: [NO_ERRORS_SCHEMA],
-    }).compileComponents();
-  }));
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [ViewAssignmentComponent],
+        schemas: [NO_ERRORS_SCHEMA],
+      }).compileComponents();
+    })
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ViewAssignmentComponent);
